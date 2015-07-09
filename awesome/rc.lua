ac74ed20dd850eaaec44a9f27c0a3226db9d00d8
Adrian Schurz <adrian.schurz@creatale.de>
2015-07-09 23:49:32 +0200

    awesome: show calendar on mouse over instead of click

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -201,16 +201,8 @@ clockwidget = wibox.widget.background()
 clockwidget:set_widget(mytextclock)
 clockwidget:set_bgimage(beautiful.widget_display)
 
-local index = 1
-local loop_widgets = { mytextclock, mytextcalendar }
-local loop_widgets_icons = { beautiful.widget_clock, beautiful.widget_cal }
-
-clockwidget:buttons(awful.util.table.join(awful.button({}, 1,
-    function ()
-        index = index % #loop_widgets + 1
-        clockwidget:set_widget(loop_widgets[index])
-        widget_clock:set_image(loop_widgets_icons[index])
-    end)))
+clockwidget:connect_signal("mouse::enter", function() clockwidget:set_widget(mytextcalendar) end)
+clockwidget:connect_signal("mouse::leave", function() clockwidget:set_widget(mytextclock) end)
 
 -- taglist
 mytaglist         = {}
